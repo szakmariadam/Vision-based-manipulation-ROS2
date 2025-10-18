9f45f5fdcdd3f6ec72f6c395483b8ea009bdb28d
Szakmariadam <Szakmari.adam@gmail.com>
2025-10-18 17:24:21 +0200

    extrinsic calibration check

--- sim/sim_vision/py/calibration_extrinsic.py
+++ sim/sim_vision/py/calibration_extrinsic.py
@@ -7,7 +7,8 @@ image_path = "calib_images/workspace.png"
 tagFamily = "tagStandard41h12"
 
 tagsize = 0.05
-square_size = tagsize - 2*(tagsize/9)
+square_size = tagsize# - 2*(tagsize/9)
+tag_id = 0
 
 tagCorners = np.array([
     [-square_size/2, square_size/2, 0],
@@ -35,12 +36,30 @@ at_detector = Detector(
 
 detection = at_detector.detect(gray)
 
-imgpoints = detection[0].corners
+imgpoints = detection[tag_id].corners
 objpoints = tagCorners
 
 retval, rvec, tvec = cv2.solvePnP(objpoints, imgpoints, K, dist, flags=cv2.SOLVEPNP_IPPE_SQUARE)
 
 print("pose found:", retval)
-print(rvec)
-print(tvec)
+if retval:
+    print("pose found for", image_path, "tag:", detection[tag_id].tag_id)
+print(rvec.ravel())
+print(tvec.ravel())
 
+axis = np.float32([
+    [0.02, 0, 0],   # X axis (red)
+    [0, 0.02, 0],   # Y axis (green)
+    [0, 0, -0.02]   # Z axis (blue)
+])
+
+imgpts, _ = cv2.projectPoints(axis, rvec, tvec, K, dist)
+
+corner = tuple(detection[tag_id].corners.mean(axis=0).astype(int))  # approximate tag center
+imgpts = np.int32(imgpts).reshape(-1, 2)
+img = cv2.line(img, corner, tuple(imgpts[0]), (0, 0, 255), 3)  # X - red
+img = cv2.line(img, corner, tuple(imgpts[1]), (0, 255, 0), 3)  # Y - green
+img = cv2.line(img, corner, tuple(imgpts[2]), (255, 0, 0), 3)  # Z - blue
+
+cv2.imshow("Image", img)
+cv2.waitKey(0)
