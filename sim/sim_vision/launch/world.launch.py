57d0a318074370379a6e35674d896ad50509839a
Szakmariadam <Szakmari.adam@gmail.com>
2025-11-04 17:17:49 +0100

    spawn camera from xacro file

--- sim/sim_vision/launch/world.launch.py
+++ sim/sim_vision/launch/world.launch.py
@@ -1,7 +1,7 @@
 from ament_index_python import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, ExecuteProcess
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, Command
 from launch_ros.substitutions import FindPackageShare
 from launch_ros.actions import Node
 import os
@@ -56,11 +56,24 @@ def generate_launch_description():
     )
 
     # Launch rviz
-    rviz_node = Node(
+    '''rviz_node = Node(
         package='rviz2',
         executable='rviz2',
         arguments=['-d', PathJoinSubstitution([get_package_share_directory('sim_vision'), 'rviz', LaunchConfiguration('rviz_config')])],
         condition=IfCondition(LaunchConfiguration('rviz')),
+        parameters=[
+            {'use_sim_time': LaunchConfiguration('use_sim_time')},
+        ]
+    )'''
+
+    spawn_camera = Node(
+        package="ros_gz_sim",
+        executable="create",
+        arguments=[
+            "-name", "camera",
+            "-topic", "robot_description",
+        ],
+        output="screen",
         parameters=[
             {'use_sim_time': LaunchConfiguration('use_sim_time')},
         ]
@@ -79,14 +92,16 @@ def generate_launch_description():
         ]
     )
 
-    spawn_camera = Node(
-        package="ros_gz_sim",
-        executable="create",
-        arguments=[
-            "-name", "camera",
-            "-file", camera_file
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        output='screen',
+        parameters=[
+            {'robot_description': Command(['xacro', ' ', camera_file]),
+             'use_sim_time': LaunchConfiguration('use_sim_time')},
         ]
-    )
+    )   
 
     return LaunchDescription([
         declare_world_arg,
@@ -95,6 +110,7 @@ def generate_launch_description():
         gz_bridge_node,
         rviz_launch_arg,
         rviz_config_arg,
-        rviz_node,
+        robot_state_publisher_node,
+        #rviz_node,
         spawn_camera
     ])
