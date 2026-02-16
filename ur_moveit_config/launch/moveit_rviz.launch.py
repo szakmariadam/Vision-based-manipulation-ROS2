f29bcc47aad4e5f25812aa70d9e6b2d2b9b9f923
Szakmariadam <Szakmari.adam@gmail.com>
2026-02-16 18:52:33 +0100

    small fixes
    catkin_pkg to requirements
    Use_sim_time to moveit_rviz.launch

--- ur_moveit_config/launch/moveit_rviz.launch.py
+++ ur_moveit_config/launch/moveit_rviz.launch.py
@@ -4,4 +4,5 @@ from moveit_configs_utils.launches import generate_moveit_rviz_launch
 
 def generate_launch_description():
     moveit_config = MoveItConfigsBuilder("ur", package_name="ur_moveit_config").to_moveit_configs()
+    moveit_config.robot_description_kinematics["use_sim_time"] = True
     return generate_moveit_rviz_launch(moveit_config)
