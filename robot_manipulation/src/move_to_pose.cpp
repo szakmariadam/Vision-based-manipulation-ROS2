843f1d9f833475d011a43ea913931d09fbe79414
Szakmariadam <Szakmari.adam@gmail.com>
2026-02-25 21:18:14 +0100

    test CollisionObject for cube on the table

--- robot_manipulation/src/move_to_pose.cpp
+++ robot_manipulation/src/move_to_pose.cpp
@@ -2,6 +2,7 @@
 
 #include <rclcpp/rclcpp.hpp>
 #include <moveit/move_group_interface/move_group_interface.hpp>
+#include <moveit/planning_scene_interface/planning_scene_interface.hpp>
 
 int main(int argc, char * argv[])
 {
@@ -31,6 +32,39 @@ int main(int argc, char * argv[])
     return msg;
     }();
     move_group_interface.setPoseTarget(target_pose);
+
+    // Create collision object for the robot to avoid
+    auto const collision_object = [frame_id =
+                                    move_group_interface.getPlanningFrame()] {
+        moveit_msgs::msg::CollisionObject collision_object;
+        collision_object.header.frame_id = frame_id;
+        collision_object.id = "cube";
+        shape_msgs::msg::SolidPrimitive primitive;
+
+        // Define the size of the box in meters
+        primitive.type = primitive.BOX;
+        primitive.dimensions.resize(3);
+        primitive.dimensions[primitive.BOX_X] = 0.05;
+        primitive.dimensions[primitive.BOX_Y] = 0.05;
+        primitive.dimensions[primitive.BOX_Z] = 0.05;
+
+        // Define the pose of the box (relative to the frame_id)
+        geometry_msgs::msg::Pose box_pose;
+        box_pose.orientation.w = 1.0;  // We can leave out the x, y, and z components of the quaternion since they are initialized to 0
+        box_pose.position.x = 0;
+        box_pose.position.y = 0;
+        box_pose.position.z = 1.05;
+
+        collision_object.primitives.push_back(primitive);
+        collision_object.primitive_poses.push_back(box_pose);
+        collision_object.operation = collision_object.ADD;
+
+        return collision_object;
+    }();
+
+    // Add the collision object to the scene
+    moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
+    planning_scene_interface.applyCollisionObject(collision_object);
 
     // Create a plan to that target pose
     auto const [success, plan] = [&move_group_interface]{
