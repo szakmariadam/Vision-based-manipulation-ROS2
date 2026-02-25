3ccf010073a61add6cd9e308a696a1495283b4ac
Szakmariadam <Szakmari.adam@gmail.com>
2026-02-25 12:59:16 +0100

    modified move_to_pose.cpp

--- robot_manipulation/src/move_to_pose.cpp
+++ robot_manipulation/src/move_to_pose.cpp
@@ -1,21 +1,49 @@
-#include <chrono>
 #include <memory>
-#include <string>
 
-#include "rclcpp/rclcpp.hpp"
-
-class MoveToPose : public rclcpp::Node
-{
-public:
-    MoveToPose() : Node("move_to_pose")
-    {
-    }
-};
+#include <rclcpp/rclcpp.hpp>
+#include <moveit/move_group_interface/move_group_interface.hpp>
 
 int main(int argc, char * argv[])
 {
+    // Initialize ROS and create the Node
     rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<MoveToPose>());
+    auto const node = std::make_shared<rclcpp::Node>(
+        "move_to_pose",
+        rclcpp::NodeOptions().automatically_declare_parameters_from_overrides(true)
+    );
+
+    auto const logger = rclcpp::get_logger("robot_manipulation");
+
+    // Create the MoveIt MoveGroup Interface
+    using moveit::planning_interface::MoveGroupInterface;
+    auto move_group_interface = MoveGroupInterface(node, "ur_arm");
+
+    // Set a target Pose
+    auto const target_pose = []{
+    geometry_msgs::msg::Pose msg;
+    msg.orientation.w = 1.0;
+    msg.position.x = 0;
+    msg.position.y = 0;
+    msg.position.z = 0.5;
+    return msg;
+    }();
+    move_group_interface.setPoseTarget(target_pose);
+
+    // Create a plan to that target pose
+    auto const [success, plan] = [&move_group_interface]{
+        moveit::planning_interface::MoveGroupInterface::Plan msg;
+        auto const ok = static_cast<bool>(move_group_interface.plan(msg));
+        return std::make_pair(ok, msg);
+    }();
+
+    // Execute the plan
+    if(success) {
+        move_group_interface.execute(plan);
+    } else {
+        RCLCPP_ERROR(logger, "Planning failed!");
+    }
+
+    // Shutdown ROS
     rclcpp::shutdown();
     return 0;
 }
