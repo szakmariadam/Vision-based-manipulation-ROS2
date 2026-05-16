71e98917dfbeacf3f2c636ea4cc76cc245189bd6
Szakmariadam <Szakmari.adam@gmail.com>
2026-05-16 15:19:32 +0200

    object manipulation controller

---
+++ object_manipulation_py/object_manipulation_py/object_manipulation_controller.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+import sys
+import termios
+import tty
+
+
+class KeyboardNode(Node):
+
+    def __init__(self):
+        super().__init__('keyboard_node')
+
+        self.publisher_ = self.create_publisher(String, 'object_manipulation', 10)
+
+        self.get_logger().info("Object manipulation controller started")
+        self.get_logger().info("Press 'b' for bottle")
+
+    def get_key(self):
+
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+
+        try:
+            tty.setraw(fd)
+            key = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+
+        return key
+
+    def run(self):
+        while rclpy.ok():
+            key = self.get_key()
+
+            msg = String()
+
+            if key == 'b':
+                self.get_logger().info("Getting bottle...")
+                msg.data = "bottle"
+                self.publisher_.publish(msg)
+
+            elif key == 'q':
+                self.get_logger().info("Quitting...")
+                break
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    node = KeyboardNode()
+
+    try:
+        node.run()
+    except KeyboardInterrupt:
+        pass
+
+    node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
